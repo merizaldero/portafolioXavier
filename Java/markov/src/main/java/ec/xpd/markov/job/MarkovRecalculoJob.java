3b26435c2a3e38784ce82cb0460cd6f5fc2eb85b
xavier <xavier@xpiderdell>
2019-01-16 21:10:33 -0500

    Calendarizacion de Job de Recalculo

--- Java/markov/src/main/java/ec/xpd/markov/job/MarkovRecalculoJob.java
+++ Java/markov/src/main/java/ec/xpd/markov/job/MarkovRecalculoJob.java
@@ -1,19 +1,25 @@
 package ec.xpd.markov.job;
 
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
+
+import ec.xpd.markov.proceso.MarkovProceso;
 
 @Component
 public class MarkovRecalculoJob extends QuartzJobBean {
 
-	
+	@Autowired
+	private MarkovProceso markovProceso;
 	
 	@Override
 	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-		
-		
+		JobDataMap jobDataMap = context.getMergedJobDataMap();
+		int idUsuario = jobDataMap.getInt("idUsuario");
+		this.markovProceso.actualizarProbabilidades(idUsuario);
 	}
 	
 }
--- Java/markov/src/main/java/ec/xpd/markov/rest/MarkovRestApi.java
+++ Java/markov/src/main/java/ec/xpd/markov/rest/MarkovRestApi.java
@@ -1,8 +1,19 @@
 package ec.xpd.markov.rest;
 
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
+import org.quartz.JobBuilder;
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ec.xpd.markov.domain.EventoTransicion;
 import ec.xpd.markov.domain.Transicion;
 import ec.xpd.markov.domain.Usuario;
+import ec.xpd.markov.job.MarkovRecalculoJob;
 import ec.xpd.markov.repository.EventoTransicionRepository;
 import ec.xpd.markov.repository.TransicionRepository;
 import ec.xpd.markov.repository.UsuarioRepository;
@@ -26,6 +38,8 @@ public class MarkovRestApi {
 	private TransicionRepository transicionRepository;
 	@Autowired
 	private EventoTransicionRepository eventoTransicionRepository;
+	
+	private Scheduler scheduler;
 	
 	@GetMapping("/opciones/{userName}/{origen}")
 	public List<Transicion> obtenerTransicionesPorUsuario(@PathVariable String userName,@PathVariable String origen){
@@ -54,7 +68,33 @@ public class MarkovRestApi {
 		
 		this.eventoTransicionRepository.save(eventoTransicion);
 		
+		this.programarRecalculoUsuario(usuario.getIdUsuario());
+		
 		return eventoTransicion;
+	}
+
+	private void programarRecalculoUsuario(Integer idUsuario) {
+		JobDataMap jobDataMap =  new JobDataMap();
+		jobDataMap.put("idUsuario", idUsuario);
+		JobDetail jobDetail = JobBuilder.newJob( MarkovRecalculoJob.class )
+				.withIdentity(UUID.randomUUID().toString(),"recalcular_job")
+				.withDescription("Recalcular Matriz de Transiciones para usuario")
+				.usingJobData(jobDataMap)
+				.storeDurably()
+				.build();
+		Trigger trigger = TriggerBuilder.newTrigger()
+				.forJob(jobDetail)
+				.withIdentity(jobDetail.getKey().getName(), "recalcular_triggers")
+				.withDescription("Recalculo de Matriz de Transiciones")
+				.startAt(new Date(System.currentTimeMillis() + 500))
+				.withSchedule(SimpleScheduleBuilder.simpleSchedule().withMisfireHandlingInstructionFireNow())
+				.build();
+		try {
+			scheduler.scheduleJob(jobDetail,trigger);
+		} catch (SchedulerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
