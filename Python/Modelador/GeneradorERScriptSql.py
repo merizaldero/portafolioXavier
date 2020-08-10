2420cd76e8c5130a3245a581217eaaaa6c723a59
xavier <xavier@xpiderdell>
2020-08-10 02:56:12 -0500

    Generador de WP MVC maduracion

--- Python/Modelador/GeneradorERScriptSql.py
+++ Python/Modelador/GeneradorERScriptSql.py
@@ -3,15 +3,15 @@ import config
 
 def getTipo(campo):
     if campo['__atributos']["tipo"] == "STRING":
-        return ("VARCHAR (%s)"%campo['__atributos']["tamano"])
+        return "varchar (%s)" % campo['__atributos']["tamano"]
     elif campo['__atributos']["tipo"] == 'INTEGER':
-        return ("INTEGER")
+        return "integer"
     elif campo['__atributos']["tipo"] == 'LONG':
-        return ("BIGINT")
+        return "bigint"
     elif campo['__atributos']["tipo"] == 'DECIMAL':
-        return ("DECIMAL (%s,%s)"%(campo['__atributos']["tamano"],campo['__atributos']["precision"]))
+        return "decimal (%s,%s)"%(campo['__atributos']["tamano"],campo['__atributos']["precision"])
     elif campo['__atributos']["tipo"] == 'BOOLEAN':
-        return ("CHAR(1)")
+        return "char(1)"
     else:
         return campo['__atributos']["tipo"]
         
@@ -28,13 +28,18 @@ def getAutonumerico(campo):
 
 def defaultTableNamer(entidad,modelo):
     prefijo = modelo['__objetoRaiz']['__atributos']['prefijo'].lower()
-    nombreTabla = entidad['__atributos']['nombreTabla']
+    nombreTabla = entidad['nombre']
+    if entidad['idTipoMetamodelo'] == 'ENTIDAD':
+        nombreTabla = entidad['__atributos']['nombreTabla']
+    elif entidad['idTipoMetamodelo'] == 'NAMED_QUERY':
+        nombreTabla = entidad['__atributos']['nombreIndice']
     return "{0}{1}".format(prefijo,nombreTabla)
 
 def obtenerCreateTable(entidad, modelo, table_namer = defaultTableNamer):    
     nombreTabla = entidad['__atributos']['nombreTabla']
     campos = entidad['__listas']['Campos']
     camposPk = [x for x in campos if x['__atributos']['pk'] == '1' ]
+    namedQueries = entidad['__listas']['NamedQuieries']
     lista_campos = []
     for campo in campos:
         nombreCampo = campo['__atributos']['nombreCampo'].lower()
@@ -44,8 +49,16 @@ def obtenerCreateTable(entidad, modelo, table_namer = defaultTableNamer):
         lista_campos.append( "\t{0} {1} {2} {3}".format( nombreCampo , tipo , obligatorio, autonumerico ) ) 
     if len(camposPk) > 0:
         lista_pk = [ campo['__atributos']['nombreCampo'].lower() for campo in camposPk ]
-        lista_campos.append( "\tPRIMARY KEY ( {0} )".format(", ".join(lista_pk)) )
-    archivo = "create table {0} (\n{1}\n);\n".format( table_namer(entidad,modelo) , ",\n".join(lista_campos) )
+        lista_campos.append( "\tPRIMARY KEY  ( {0} )".format(", ".join(lista_pk)  ) )
+    for namedQuery in namedQueries:
+        unique_flag = ''
+        campos_where = namedQuery['__listas']['camposWhere']
+        lista_campos_key = [ "{0}".format( campo['__atributos']['nombreCampo'].lower() ) for campo_where in campos_where for campo in campos if campo['nombre'] == campo_where['nombre']  ]
+        if namedQuery['__atributos']['indiceUnico'] == '1':
+            unique_flag = 'UNIQUE '
+        if len(lista_campos_key) > 0:
+            lista_campos.append( "\t{0}KEY {1} ({2})".format( unique_flag, table_namer(namedQuery,modelo), ", ".join(lista_campos_key) ) )
+    archivo = "CREATE TABLE {0} (\n{1}\n)".format( table_namer(entidad,modelo) , ",\n".join(lista_campos) )
     return archivo
             
 def generarModelo(modelo):
--- Python/Modelador/GeneradorWpMvc.py
+++ Python/Modelador/GeneradorWpMvc.py
@@ -1,4 +1,5 @@
 import GeneradorERScriptSql
+from email._header_value_parser import get_display_name
 
 ESPACIO = ' '*4
 
@@ -32,22 +33,65 @@ TIPOS = {
     'FK_USER': { 'pattern': r'/^(\+|-)?\d+$/' },
 }
 
+INPUT_MAPPINGS = {
+    'STRING': { 'type':'text' , 'maxlenght':'$tamano' },
+    'INTEGER': { 'pattern': r'/^(\+|-)?\d+$/' },
+    'LONG': { 'pattern': r'/^(\+|-)?\d+$/' },
+    'DECIMAL': { 'rule':'numeric' },
+    'FLOAT': { 'rule':'numeric' },
+    'DOUBLE': { 'rule':'numeric' },
+    'DATE': { 'pattern': r'/^([1-9][0-9]{3})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])?$/' },
+    'BOOLEAN': { 'pattern': r'/^1|0$/' },
+    'CLOB': {  },
+    'BLOB': {  },
+    'DATETIME': { 'pattern': r'/^(?:[\+-]?\d{4}(?!\d{2}\b))(?:(-?)(?:(?:0[1-9]|1[0-2])(?:\1(?:[12]\d|0[1-9]|3[01]))?|W(?:[0-4]\d|5[0-2])(?:-?[1-7])?|(?:00[1-9]|0[1-9]\d|[12]\d{2}|3(?:[0-5]\d|6[1-6])))(?:[T\s](?:(?:(?:[01]\d|2[0-3])(?:(:?)[0-5]\d)?|24\:?00)(?:[\.,]\d+(?!:))?)?(?:\2[0-5]\d(?:[\.,]\d+)?)?(?:[zZ]|(?:[\+-])(?:[01]\d|2[0-3]):?(?:[0-5]\d)?)?)?)?$/' },
+    'DURATION': { 'pattern': r'/^(-?)P(?=\d|T\d)(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)([DW]))?(?:T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+(?:\.\d+)?)S)?)?$/' },
+    'EMAIL': { 'rule':'email' },
+    'FILE': {  },
+    'FILEPATH': { 'pattern': r'/(\\\\?([^\\/]*[\\/])*)([^\\/]+)$/' },
+    'IMAGE': {  },
+    'GEN_IP_ADDRESS': { 'pattern': r'/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/' },
+    'NULL_BOOLEAN': { 'pattern': r'/^1|0$/' },
+    'POS_INTEGER': { 'pattern': r'/^\d+$/' },
+    'POS_SM_INTEGER': { 'pattern': r'/^\d+$/' },
+    'SLUG': { 'rule':'not_empty' },
+    'SM_INTEGER': { 'pattern': r'/^(\+|-)?\d+$/' },
+    'TIME': { 'pattern': r'/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/' },
+    'URL': { 'rule':'url' },
+    'UUID': { 'pattern': r'/^[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}$/' },
+    'FK': {  },
+    'FK_USER': { 'pattern': r'/^(\+|-)?\d+$/' },
+}
+
+def generarSecuenciaGen(modelo,generacion):
+    nombrePlugin = modelo['__objetoRaiz']['nombre']
+    entidades = modelo['__objetoRaiz']['__listas']['Entidades']
+    lista_entidades = ["sudo ./wpmvc generate {2} {1} {0}".format(entidad['nombre'], nombrePlugin, generacion) for entidad in entidades]
+    return "\n".join( lista_entidades )
+
 def generarInitSequence(modelo):
     contenido = ''
     nombrePlugin = modelo['__objetoRaiz']['nombre']
-    entidades = modelo['__objetoRaiz']['__listas']['Entidades']
-    lista_entidades = ["sudo ./wpmvc generate model {1} {0}".format(entidad['nombre'], nombrePlugin) for entidad in entidades]
+    entidades = modelo['__objetoRaiz']['__listas']['Entidades']    
     contenido = """
 cd /var/www/html/wp-content/plugins/wp-mvc
 sudo ./wpmvc generate plugin {0}
 
 {1}
-    """.format(nombrePlugin, "\n".join(lista_entidades))
+    """.format(nombrePlugin, generarSecuenciaGen(modelo, 'model') )
     return contenido
 
-def tableNamer(entidad,modelo):    
+def tableNamer(entidad,modelo):
     nombreTabla = entidad['nombre'].lower()
-    return "' . $this->tables['" + nombreTabla + "s'] . '"
+    prefijo = modelo['__objetoRaiz']['__atributos']['prefijo']
+    if entidad['idTipoMetamodelo'] == 'ENTIDAD':
+        nombreTabla = entidad['__atributos']['nombreTabla']
+        return "' . $wpdb->prefix . '" + prefijo + nombreTabla 
+    elif entidad['idTipoMetamodelo'] == 'NAMED_QUERY':
+        prefijo = modelo['__objetoRaiz']['__atributos']['prefijo'].lower()
+        nombreTabla = entidad['__atributos']['nombreIndice'].lower()
+        return "{0}{1}".format(prefijo,nombreTabla)
+    return nombreTabla
 
 def generarPluginLoader(modelo):
     scriptbdd = GeneradorERScriptSql.generarModelo(modelo)
@@ -67,6 +111,8 @@ function activate() {
     
     add_option('my_plugin_db_version', $this->db_version);
     
+    global $wpdb;
+    
     $sqls = [
     """ + ",\n".join(createSqls) + """
     ];
@@ -78,6 +124,17 @@ function activate() {
     """
     return contenido
 
+def obtenerCamposPksFks(entidad,entidades):
+    campos = entidad['__listas']['Campos']
+    pk = [ campo for campo in campos if campo['__atributos']['pk'] == '1' ][0]
+    fks = [ (named_query, entidad_fk) 
+           for named_query in entidad['__listas']['NamedQuieries'] 
+           for entidad_fk in entidades 
+           if named_query['__atributos']['entidadFK'].isnumeric() 
+           and entidad_fk['idObjeto'] == int(named_query['__atributos']['entidadFK']) ]
+    display_name = [ campo for campo in campos if campo['__atributos']['displayName'] == '1' ][0]
+    return campos, pk, fks, display_name
+
 def anexarModelClases(modelo):
     resultado = []
     nombrePlugin = modelo['__objetoRaiz']['nombre']
@@ -85,8 +142,9 @@ def anexarModelClases(modelo):
     entidades = modelo['__objetoRaiz']['__listas']['Entidades']
     for entidad in entidades:
         nombreTabla = "{prefix}" + prefijo + entidad['__atributos']['nombreTabla']
-        campos = entidad['__listas']['Campos']
-        pk = [ campo for campo in campos if campo['__atributos']['pk'] == '1' ][0]
+        
+        campos, pk, fks , display_name  = obtenerCamposPksFks(entidad,entidades)        
+        
         lista_campos = ["'{0}'".format( campo['__atributos']['nombreCampo'].lower() ) for campo in campos]
         validaciones_campos = []
         for campo in campos:
@@ -101,7 +159,15 @@ def anexarModelClases(modelo):
             tipo = TIPOS[ campo['__atributos']['tipo'] ]
             atributos_validacion += [ "'{0}' => '{1}'".format( clave, tipo[clave] ) for clave in list(tipo.keys()) ]
             validaciones_campos.append("'{0}' => [\n            {1}\n        ]".format( nombreCampo, ",\n            ".join(atributos_validacion) ))
-        path = "plugins/{0}/app/models/{1}.php".format( nombrePlugin, entidad['nombre'].lower() )
+        lista_fks = []
+        for fkdef in fks:
+            named_query = fkdef[0]
+            entidad_fk = fkdef[1]
+            campos_fk = [campo for campo_fk in named_query['__listas']['camposWhere'] for campo in campos 
+                         if campo['nombre'] == campo_fk['nombre']]
+            lista_campos_fk = [ campo['__atributos']['nombreCampo'].lower() for campo in campos_fk ]
+            lista_fks.append("'{0}' => [ 'foreign_key' => '{1}' ]".format( entidad_fk['nombre'], ",".join(lista_campos_fk) ) )
+        path = "sudo nano ../{0}/app/models/{1}.php".format( nombrePlugin, entidad['nombre'].lower() )
         contenido ="""<?php
 class """+entidad['nombre']+""" extends MvcModel {
     var $table = '""" + nombreTabla + """';    
@@ -110,18 +176,193 @@ class """+entidad['nombre']+""" extends MvcModel {
     var $validate = [
         """ + ",\n        ".join(validaciones_campos) + """
     ];
+    var $belongs_to = [
+        """ + ",\n        ".join(lista_fks) + """
+    ];
     var $per_page = 7;
+    var $display_name = '"""+ display_name['__atributos']['nombreCampo'].lower() +"""';
 }
 ?>"""
         resultado.append( { "path" : path , "contenido" : contenido } )
     return resultado
+
+def anexarControllerClases(modelo):
+    resultado = []
+    nombrePlugin = modelo['__objetoRaiz']['nombre']
+    prefijo = modelo['__objetoRaiz']['__atributos']['prefijo']
+    entidades = modelo['__objetoRaiz']['__listas']['Entidades']
+    for entidad in entidades:
+        nombreTabla = "{prefix}" + prefijo + entidad['__atributos']['nombreTabla']
+        
+        campos, pk, fks, display_name = obtenerCamposPksFks(entidad,entidades) 
+        
+        lista_campos = ["'{0}'".format( campo['__atributos']['nombreCampo'].lower() ) for campo in campos]
+        default_columns = []
+        default_column_methods = []
+        for campo in campos:
+            nombreCampo = campo['__atributos']['nombreCampo'].lower()
+            default_columns.append("'{0}' => [\n            'label'=>'{2}',\n            'value_method'=>'print_{1}'\n        ]".format( nombreCampo, campo['nombre'], campo['nombre'].capitalize() ))
+            default_column_methods.append("public function print_" + campo['nombre'] + " ($object) {\n        return empty($object->"+nombreCampo+") ? null : $object->"+nombreCampo+" ; \n    }\n")
+
+        lista_fk_methods = []
+        lista_fk_method_calls = []
+        for fkdef in fks:
+            #named_query = fkdef[0]
+            entidad_fk = fkdef[1]
+            campos_fk, pk_fk, fks_fk, display_name_fk = obtenerCamposPksFks(entidad_fk,entidades)
+            campos_nombre = [ "'{0}'".format(pk_fk['__atributos']['nombreCampo'].lower()) , "'{0}'".format(display_name_fk['__atributos']['nombreCampo'].lower()) ]
+            lista_fk_methods.append("""private function set_""" + entidad_fk['nombre'].lower() + """s() {
+        $this->loadModel('""" + entidad_fk['nombre'] + """');
+        $"""+ entidad_fk['nombre'].lower() +"""s = $this->""" + entidad_fk['nombre'] + """->find( ['selects' => ["""+", ".join(campos_nombre)+"""] ] );
+        $this->set('"""+ entidad_fk['nombre'].lower() +"""s', $"""+ entidad_fk['nombre'].lower() +"""s );
+    }
+""" )
+            lista_fk_method_calls.append("$this->set_{0}s();".format( entidad_fk['nombre'].lower() ))
+        # Admin Controller
+        path = "sudo nano ../{0}/app/controllers/admin/admin_{1}s_controller.php".format( nombrePlugin, entidad['nombre'].lower() )
+        contenido ="""<?php
+class Admin"""+entidad['nombre']+"""sController extends MvcAdminController {
+    var $default_columns = [
+        """ + ",\n        ".join(default_columns) + """
+    ];
+"""+ "\n        ".join(default_column_methods) +"""
+""" + "\n".join(lista_fk_methods) + """
+
+    public function add(){
+        
+        $this->set_object();
+        $this->create();
+    }
     
+    public function edit(){
+        """+ "\n".join(lista_fk_method_calls) +"""
+        $this->verify_id_param();
+        $this->set_object();
+        $this->create_or_save();
+    }
+}
+?>"""
+        resultado.append( { "path" : path , "contenido" : contenido } )
+        
+        # Public Controller
+        path = "sudo nano ../{0}/app/controllers/{1}s_controller.php".format( nombrePlugin, entidad['nombre'].lower() )
+        contenido ="""<?php
+class """+entidad['nombre']+"""sController extends MvcPublicController {
+    var $default_columns = [
+        """ + ",\n        ".join(default_columns) + """
+    ];
+"""+ "\n        ".join(default_column_methods) +"""
+""" + "\n".join(lista_fk_methods) + """   
+}
+?>"""
+        resultado.append( { "path" : path , "contenido" : contenido } )
+
+    return resultado
+
+def contenidoShowView( entidad, entidades ):
+    
+    campos, pk, fks, display_name = obtenerCamposPksFks(entidad,entidades)
+    
+    lista_campos = []
+    for campo in campos:
+        if campo['nombre'] == pk['nombre'] or campo['__atributos']['displayName'] == '1' :
+            continue
+        nombreCampo = campo['__atributos']['nombreCampo']
+        lista_campos.append( """<$php
+if(!empty($object->""" + nombreCampo.lower() + """)){
+    echo '<div><span>"""+ campo['nombre'] +"""</span><span>' . $object->""" + nombreCampo.lower() + """ . '</span></div>';
+} 
+?>""" )   
+    
+    contenido = """
+
+<h2><?php echo $object->"""+ display_name['__atributos']['nombreCampo'].lower() +"""; ?></h2>
+
+"""+ "\n".join(lista_campos)+"""
+
+<p>
+  <?php echo $this->html->link('Todos los """ + entidad['nombre'] + """s', array('controller' => '""" + entidad['nombre'].lower() + """s')); ?>
+</p>
+    
+    """
+    return contenido
+
+def contenidoAdminAddView( entidad, entidades ):
+    campos, pk, fks, display_name = obtenerCamposPksFks(entidad,entidades)
+    lista_campos = []
+    for campo in campos:
+        if campo['nombre'] == pk['nombre'] :
+            continue
+        nombreCampo = campo['__atributos']['nombreCampo']
+        lista_campos.append( "<?php echo $this->form->input('{0}', ['label' => '{1}']); ?>".format( nombreCampo.lower(), campo['nombre'].capitalize() ) )        
+    contenido = """<h2>Nuevo """ + entidad['nombre'] + """</h2>
+
+<?php echo $this->form->create($model->name, array('is_admin' => $this->is_admin)); ?>
+<?php echo $this->form->open_admin_table(); ?>
+
+""" + "\n".join(lista_campos) + """
+
+<?php echo $this->form->close_admin_table(); ?>
+<?php echo $this->form->end('Guardar'); ?>
+"""
+
+    return contenido
+
+def contenidoAdminEditView( entidad, entidades ):
+    campos, pk, fks, display_name = obtenerCamposPksFks(entidad,entidades)
+    lista_campos = []
+    for campo in campos:
+        if campo['nombre'] == pk['nombre']:
+            continue
+        nombreCampo = campo['__atributos']['nombreCampo']        
+        lista_campos.append( "<?php echo $this->form->input('{0}', ['label' => '{1}']); ?>".format( nombreCampo.lower(), campo['nombre'].capitalize() ) )        
+    contenido = """<h2>Editar """ + entidad['nombre'] + """</h2>
+
+<?php echo $this->form->create($model->name, array('is_admin' => $this->is_admin)); ?>
+<?php echo $this->form->open_admin_table(); ?>
+
+""" + "\n".join(lista_campos) + """
+
+<?php echo $this->form->close_admin_table(); ?>
+<?php echo $this->form->end('Actualizar'); ?>
+"""
+
+    return contenido
+
+
+def anexarPages(modelo):
+    resultado = []
+    nombrePlugin = modelo['__objetoRaiz']['nombre']
+    prefijo = modelo['__objetoRaiz']['__atributos']['prefijo']
+    entidades = modelo['__objetoRaiz']['__listas']['Entidades']
+    
+    resultado += [ { "path": "sudo vim ../{0}/app/views/{1}s/show.php".format( nombrePlugin, entidad['nombre'].lower() ) , 
+                    "contenido": contenidoShowView( entidad, entidades ) }
+        for entidad in entidades
+        ]
+    
+    resultado += [ { "path": "sudo vim ../{0}/app/views/admin/{1}s/add.php".format( nombrePlugin, entidad['nombre'].lower() ) , 
+                    "contenido": contenidoAdminAddView( entidad, entidades ) }
+        for entidad in entidades
+        ]
+
+    resultado += [ { "path": "sudo vim ../{0}/app/views/admin/{1}s/edit.php".format( nombrePlugin, entidad['nombre'].lower() ) , 
+                    "contenido": contenidoAdminEditView( entidad, entidades ) }
+        for entidad in entidades
+        ]
+
+    return resultado
+
 def generarModelo(modelo):
     nombrePlugin = modelo['__objetoRaiz']['nombre']
     resultado = { 'archivos' : [ 
-                                 { 'path':'Secuencia init',     'contenido': generarInitSequence(modelo) },
-                                 { 'path':'plugins/{0}/{0}_loader.php'.format( nombrePlugin ), 'contenido': generarPluginLoader(modelo) },
+                                 { 'path':'SECUENCIA INICIAL',     'contenido': generarInitSequence(modelo) },
+                                 { 'path':'sudo nano ../{0}/{0}_loader.php'.format( nombrePlugin ), 'contenido': generarPluginLoader(modelo) },
                                  ]}
-    resultado['archivos'] +=  anexarModelClases(modelo) ;
+    resultado['archivos'] +=  anexarModelClases(modelo)
+    resultado['archivos'].append( {'path' : 'SECUENCIA GENERACION CONTROLLERS', 'contenido' : generarSecuenciaGen(modelo,'controllers') } )
+    resultado['archivos'] +=  anexarControllerClases(modelo)
+    resultado['archivos'].append( {'path' : 'SECUENCIA GENERACION VIEWS', 'contenido' : generarSecuenciaGen(modelo,'views') } )
+    resultado['archivos'] +=  anexarPages(modelo)
     return resultado
     
